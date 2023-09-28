018effaf4610fa011e000b5057c5984283d0a852
Marcus Leung <82466170+marcus-leung@users.noreply.github.com>
2023-09-27 21:01:42 -0700

    Add FAQ section and component

---
+++ src/components/Dropdown.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { useState, useRef } from "react";
+
+function Dropdown(props) {
+  //props.label , props.children (built in)
+  const [open, setOpen] = useState(false);
+
+  const toggle = () => {
+    setOpen(!open);
+  };
+
+  const contentRef = useRef();
+
+  return (
+    <div className="dropdown">
+      <div className="row">
+        <button onClick={toggle} className="collapsible text-center pl-4">
+          {props.label}
+          
+          <i className="fa-solid fa-chevron-down "></i>
+        </button>
+        <div className="content-parent"
+          ref={contentRef} style=
+          {open
+            ? { height: contentRef.current.scrollHeight + "px" }
+            : { height: "0px" }}>
+            <div className="content text-center">{props.children}</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Dropdown;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Dropdown from "../components/Dropdown";
 import image from "../assets/YearlyPhotos/2022-2023/22.23.2.jpg";
 import discord from "../assets/discord.png";
 import { Link } from "react-router-dom";
@@ -11,9 +12,11 @@ function Home() {
   return (
     <div className="home">
       <div className="container py-5">
-        <div className="row align-items-center my-5" style={{width: "100%"}}>
+        <div className="row align-items-center my-5" style={{ width: "100%" }}>
           <div className="col-lg-5 fadeInLeft">
-            <h1 className="font-weight-light"><center>{homeText.title}</center></h1>
+            <h1 className="font-weight-light">
+              <center>{homeText.title}</center>
+            </h1>
             <p className="lead pl-2">{homeText.description}</p>
           </div>
           <div className="col-lg-7 pl-0 fadeInRight">
@@ -49,13 +52,34 @@ function Home() {
       <div className="container">
         <div className="row align-items-center my-5">
           <div className="col-lg-4">
-            <img className="img-fluid rounded" style={{border: "7px solid black"}} src={Jinsung} alt="president" />
+            <img
+              className="img-fluid rounded"
+              style={{ border: "7px solid black" }}
+              src={Jinsung}
+              alt="president"
+            />
           </div>
           <div className="col-lg">
             <h1>{homeText.title2}</h1>
             <p>{homeText.description3}</p>
             <p>{homeText.description4}</p>
           </div>
+        </div>
+      </div>
+      <div className="bg-yellow py-4">
+        <div className="container my-5">
+          <div className="section-title row justify-content-center">
+            <h1>FAQ</h1>
+          </div>
+            <Dropdown label="test">
+              <p>answer</p>
+            </Dropdown>
+            <Dropdown label="test">
+              <p>answer</p>
+            </Dropdown>
+            <Dropdown label="test">
+              <p>answer</p>
+            </Dropdown>
         </div>
       </div>
     </div>
