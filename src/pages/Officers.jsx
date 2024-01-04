74d6afdff12525f254a35fa0538912b2ea486542
Marcus Leung <82466170+marcus-leung@users.noreply.github.com>
2024-01-04 14:16:43 -0800

    Update officers page

--- src/pages/Officers.jsx
+++ src/pages/Officers.jsx
@@ -3,41 +3,108 @@ import Person from "../components/Person";
 import Brian from "../assets/MemberPhotos/Brian.png";
 import Jinsung from "../assets/MemberPhotos/Jinsung.png";
 import Alayna from "../assets/MemberPhotos/Alayna.png";
-import Marcus from  "../assets/MemberPhotos/Marcus.jpg";
+import Marcus from "../assets/MemberPhotos/Marcus.jpg";
 import Trevor from "../assets/MemberPhotos/Trevor.png";
-import Tensi from "../assets/MemberPhotos/Tensi.png";
 import Alex from "../assets/MemberPhotos/Alex.png";
+import Ryan from "../assets/MemberPhotos/Ryan.png";
+import Josephine from "../assets/MemberPhotos/Josephine.png";
+import MJ from "../assets/MemberPhotos/MJ.png";
 
 function Members() {
   return (
     <div className="officers">
       <div className="container">
-        <div className="row pt-5">
-          <div className="col-3" align="center">
+        <div className="row pt-5 my-5">
+          <div
+            className="col-lg-3 col-md-4 col-sm-6 col-12 mb-4"
+            align="center"
+          >
             <Person img={Brian} name="Brian Shen" role="President" />
           </div>
-          <div className="col-3" align="center">
+          <div
+            className="col-lg-3 col-md-4 col-sm-6 col-12 mb-4"
+            align="center"
+          >
             <Person img={Jinsung} name="Jinsung Park" role="Vice President" />
           </div>
-          <div className="col-3" align="center">
-            <Person img={Alayna} name="Alayna Rangel" role="Director of External Affairs" />
+          <div
+            className="col-lg-3 col-md-4 col-sm-6 col-12 mb-4"
+            align="center"
+          >
+            <Person
+              img={Alayna}
+              name="Alayna Rangel"
+              role="Director of External Affairs"
+            />
           </div>
-          <div className="col-3" align="center">
-            <Person img={Marcus} name="Marcus Leung" role="Web Developer | Officer of External Affairs" />
+          <div
+            className="col-lg-3 col-md-4 col-sm-6 col-12 mb-4"
+            align="center"
+          >
+            <Person
+              img={Marcus}
+              name="Marcus Leung"
+              role="Web Developer | Officer of External Affairs"
+            />
           </div>
-        </div>
-        <div className="row py-5">
-          <div className="col-3" align="center">
-            <Person img={Trevor} name="Trevor Le" role="Officer of External Affairs" />
+
+          <div
+            className="col-lg-3 col-md-4 col-sm-6 col-12 mb-4"
+            align="center"
+          >
+            <Person name="Kevin Habek" role="Director of Content" />
           </div>
-          <div className="col-3" align="center">
-            <Person name="Kevin" role="Director of Content" />
-          </div>
-          <div className="col-3" align="center">
+          <div
+            className="col-lg-3 col-md-4 col-sm-6 col-12 mb-4"
+            align="center"
+          >
             <Person img={Alex} name="Alex Kosan" role="Equity Analyst Lead" />
           </div>
-          <div className="col-3" align="center">
-            <Person name="Bruno" role="Venture Capital Lead" />
+          <div
+            className="col-lg-3 col-md-4 col-sm-6 col-12 mb-4"
+            align="center"
+          >
+            <Person name="Bruno Madrid" role="Venture Capital Lead" />
+          </div>
+          <div
+            className="col-lg-3 col-md-4 col-sm-6 col-12 mb-4"
+            align="center"
+          >
+            <Person
+              img={Trevor}
+              name="Trevor Le"
+              role="Officer of External Affairs"
+            />
+          </div>
+          <div
+            className="col-lg-3 col-md-4 col-sm-6 col-12 mb-4"
+            align="center"
+          >
+            <Person
+              img={Josephine}
+              name="Josephine Wang"
+              role="Officer of External Affairs"
+            />
+          </div>
+          <div
+            className="col-lg-3 col-md-4 col-sm-6 col-12 mb-4"
+            align="center"
+          >
+            <Person
+              img={Ryan}
+              name="Ryan Ni"
+              role="Officer of External Affairs"
+            />
+          </div>
+          <div
+            className="col-lg-3 col-md-4 col-sm-6 col-12 mb-4"
+            align="center"
+          >
+            <Person
+              img={MJ}
+              name="M. Jahra"
+              role="Officer of External Affairs"
+            />
           </div>
         </div>
       </div>
