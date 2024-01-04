5e036dec7435ca3cc24ff42562f492c21e003e16
Marcus Leung <82466170+marcus-leung@users.noreply.github.com>
2024-01-04 14:17:01 -0800

    Add alumn company

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,6 +4,7 @@ import texts from "../data/texts";
 import amazon from "../assets/Companies/amazon.png";
 import adam from "../assets/Companies/mossadam.png";
 import apple from "../assets/Companies/apple.png";
+import jpmorgan from "../assets/Companies/jpmorgan.png";
 
 function About() {
   const aboutText = texts.about;
@@ -29,8 +30,9 @@ function About() {
         <h1>Alumni Placements</h1>
         <div className="col">
           <img className="img-fluid m-2" src={adam} alt="Moss Adams" width="40%"/>
-          <img className="img-fluid m-2" src={apple} alt="Moss Adams" width="40%"/>
-          <img className="img-fluid m-2" src={amazon} alt="Moss Adams" width="40%"/>
+          <img className="img-fluid m-2 p-2" src={apple} alt="Moss Adams" width="40%"/>
+          <img className="img-fluid m-2 p-4" src={amazon} alt="Moss Adams" width="40%"/>
+          <img className="img-fluid m-2 pb-4" src={jpmorgan} alt="J.P. Morgan" width="40%"/>
         </div>
       </div>
     </div>
