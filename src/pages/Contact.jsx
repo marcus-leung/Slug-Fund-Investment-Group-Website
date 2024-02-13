cc493adec792fd6e11aa5c9638092e4c619ab186
Marcus Leung <82466170+marcus-leung@users.noreply.github.com>
2024-02-12 23:29:49 -0800

    Add linkedin

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -15,9 +15,10 @@ function Contact() {
           <div className="col-7 py-5 my-5 fadeInRight">
           <h1 className="py-2 font-weight-bold" style={{fontSize: "50px"}}>Get In Touch</h1>
           <p style={{fontSize: "25px"}}>Email: slugsfundig@gmail.com</p>
-          <p style={{fontSize: "25px"}}>Instagram: <a href="https://www.instagram.com/ucsc.sfig/" target="_blank" rel="noopener noreferrer">https://www.instagram.com/ucsc.sfig/</a></p>
-          <p style={{fontSize: "25px"}}>Twitter: <a href="https://twitter.com/SFIG_UCSC" target="_blank" rel="noopener noreferrer">https://twitter.com/SFIG_UCSC</a></p>
-          <p style={{fontSize: "25px"}}>Discord: <a href="https://discord.gg/zcm5rgkrRS" target="_blank" rel="noopener noreferrer">https://discord.gg/zcm5rgkrRS</a></p>
+          <p style={{fontSize: "25px"}}>Instagram: <a href="https://www.instagram.com/ucsc.sfig/" target="_blank" rel="noopener noreferrer">www.instagram.com/ucsc.sfig/</a></p>
+          <p style={{fontSize: "25px"}}>Twitter: <a href="https://twitter.com/SFIG_UCSC" target="_blank" rel="noopener noreferrer">twitter.com/SFIG_UCSC</a></p>
+          <p style={{fontSize: "25px"}}>Discord: <a href="https://discord.gg/zcm5rgkrRS" target="_blank" rel="noopener noreferrer">discord.gg/zcm5rgkrRS</a></p>
+          <p style={{fontSize: "25px"}}>Linkedin: <a href="https://www.linkedin.com/school/slugfundinvestmentgroup/" target="_blank" rel="noopener noreferrer">www.linkedin.com/school/slugfundinvestmentgroup/</a></p>
           <p style={{fontSize: "30px"}}>Join the club by joining the discord!</p>
           </div>
         </div>
